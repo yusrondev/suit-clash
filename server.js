a9a1e378055d10b8fc879bdea300b01256ca8c16
Yuz Dev <yusron.dev@gmail.com>
2026-04-30 21:02:09 +0700

    DEV - fix ip

--- server.js
+++ server.js
@@ -1288,5 +1288,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT ||3001;
-server.listen(PORT, () => console.log(`Server: http://localhost:${PORT}`));
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
